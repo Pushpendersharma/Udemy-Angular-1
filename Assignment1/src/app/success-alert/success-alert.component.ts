2151d24ea10d33575e0f4146295fa8864e79634c
PUSHPENDER SHARMA <rishu.sharma.814@gmail.com>
2023-09-28 20:36:54 +0530

    fixes

--- Assignment1/src/app/success-alert/success-alert.component.ts
+++ Assignment1/src/app/success-alert/success-alert.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
-  template: '<p class="success">Success, Everything is fine!',
-  styles: [`.success{
+  template: '<h5>Success Alert</h5><p class="success">Success, Everything is fine!',
+  styles: [`.success,h5{
     color : green;
+  }
+  p{
+    padding:20px;
+    background-color:rgb(211, 165, 38)
   }`]
 })
 export class SuccessAlertComponent {
--- Assignment1/src/app/warning-alert/warning-alert.component.ts
+++ Assignment1/src/app/warning-alert/warning-alert.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  template: '<p class="warning">Warning, Something is wrong!',
-  styles: [`.warning{
+  template: '<h5>Warning Alert</h5><p class="warning">Warning, Something is wrong!',
+  styles: [`.warning,h5{
     color : red;
+  }
+  p{
+    padding:20px;
+    background-color:rgb(211, 165, 38)
   }`]
 })
 export class WarningAlertComponent {
